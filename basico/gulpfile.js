6703f03524fcead2c15479cde5b6502653b4bd12
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-08-19 15:33:34 -0300

    Full Commit

---
+++ basico/gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
+// const series = gulp.series;
+
+const antes1 = callback => {
+    console.log('Tarefa Antes 1');
+    return callback()
+}
+
+const antes2 = callback => {
+    console.log('Tarefa Antes 2');
+    return callback()
+}
+
+function copiar(callback) {
+    gulp.src('pastaA/**/*.txt').pipe(gulp.dest('pastaB'));
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']).pipe(gulp.dest('pastaB'));
+
+    //O comando pipe essencialmente funciona como .then
+    return callback()
+}
+
+const fim = callback => {
+    console.log('Tarefa Fim');
+    return callback()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim
+);
